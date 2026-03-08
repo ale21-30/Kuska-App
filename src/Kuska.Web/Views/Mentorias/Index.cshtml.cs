6c5b91abaa9fca21f5254a07fd7de5ceb79a9e5b
ale21-30 <ale512127@gmail.com>
2026-03-07 19:23:51 -0500

    vistas Login, Registro, Fondos y Mentorias

---
+++ src/Kuska.Web/Views/Mentorias/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kuska.Web.Views.Mentorias
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
