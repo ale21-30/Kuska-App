d6ede74d7b9b3e7eee6ace3e9ef831e4c1635371
ale21-30 <ale512127@gmail.com>
2026-03-07 21:10:59 -0500

    vistas Agendar, MisSesiones funcionando con datos reales

---
+++ src/Kuska.Web/Views/Mentorias/Agendar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kuska.Web.Views.Mentorias
+{
+    public class AgendarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/Kuska.Web/Views/Mentorias/MisSesiones.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kuska.Web.Views.Mentorias
+{
+    public class MisSesionesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
